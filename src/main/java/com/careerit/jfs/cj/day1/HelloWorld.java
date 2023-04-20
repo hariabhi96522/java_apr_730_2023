242f35835f448b5d94c3586ba8db9e32e89e37b5
hariabhi96522 <areteharikareddy@gmail.com>
2023-04-20 13:46:19 +0530

    added extra log information

--- src/main/java/com/careerit/jfs/cj/day1/HelloWorld.java
+++ src/main/java/com/careerit/jfs/cj/day1/HelloWorld.java
@@ -2,5 +2,9 @@ package com.careerit.jfs.cj.day1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println("hi good"+i);
+        }
     }
 }
